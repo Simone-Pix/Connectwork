2516338dc4185ce9a4f3ce471456175902fa8c2c
Vittorio Cenni <Vittorio.Cenni@edu-its.it>
2025-11-11 16:01:41 +0100

    aggiunta struttura iniziale

---
+++ frontend/src/pages/Search.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar";
+
+function Search() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the Search Page</h1>
+    </div>
+  );
+}
+
+export default Search;
