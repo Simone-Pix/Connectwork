5e7d4ff1c11a6d0c9244ee3f9a79200694ca7e86
Omarben05 <your-email@example.com>
2025-11-11 10:28:28 +0100

    Creazione dei services per gli users e valutazioni

---
+++ backend/src/main/java/com/immobiliaris/backend/service/UserService.java
@@ -0,0 +1,13 @@
+package com.immobiliaris.backend.service;
+import java.util.List;
+
+import com.immobiliaris.backend.model.Users;
+
+public interface UserService {
+    Users createUser(Users user);
+    Users save(Users user);
+    List<Users> getAllUsers();
+    Users getUserById(Long id);
+    Users updateUser(Long id, Users user);
+    Users deleteUser(Long id);
+}
