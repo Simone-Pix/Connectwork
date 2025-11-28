ff9c009f008a305e2618c61de50ca967963241e9
Simone-Pix <simone.pizzorno@edu-its.it>
2025-11-28 02:02:26 +0100

    Fix richiesteID&Valutazioneautomatica route

--- backend/src/main/java/com/immobiliaris/backend/repo/ValutazioniRepository.java
+++ backend/src/main/java/com/immobiliaris/backend/repo/ValutazioniRepository.java
@@ -3,7 +3,10 @@ package com.immobiliaris.backend.repo;
 import com.immobiliaris.backend.model.Valutazioni;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface ValutazioniRepository extends JpaRepository<Valutazioni, Long> {
     List<Valutazioni> findByImmobileId(Long immobileId);
+    Optional<Valutazioni> findByRichiestaId(Long richiestaId);
+    List<Valutazioni> findAllByRichiestaId(Long richiestaId);
 }
