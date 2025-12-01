e99a937c5da1cbd2e4609c1378b5edf556a043db
Vittorio <vittorio.cenni.02@gmail.com>
2025-12-01 16:38:46 +0100

    sistemato menu mobile del backoffice

--- frontend/src/pages/Backoffice.jsx
+++ frontend/src/pages/Backoffice.jsx
@@ -349,7 +349,17 @@ function Backoffice() {
         className="backoffice-mobileToggle-backoffice lg:hidden"
         onClick={() => setMobileOpen(!mobileOpen)}
       >
-        Backoffice pages
+        <span>{mobileOpen ? "Chiudi Menu" : "Backoffice Pages"}</span>
+        <svg 
+          xmlns="http://www.w3.org/2000/svg" 
+          fill="none" 
+          viewBox="0 0 24 24" 
+          strokeWidth={2} 
+          stroke="currentColor" 
+          className={`w-5 h-5 transition-transform ${mobileOpen ? 'rotate-180' : ''}`}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
       </div>
 
       <div className={`backoffice-layout-backoffice ${mobileOpen ? "open" : ""}`}>
